7ffc15c327e15e02a879ffce5cd8c9aaf98afb1e
nasubi-dev <r916nis@gmail.com>
2026-01-26 21:23:44 +0900

    謎

--- src/client/app/root.tsx
+++ src/client/app/root.tsx
@@ -38,7 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-51GNEV2RM0"></script>
         <script dangerouslySetInnerHTML={{__html: `
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-51GNEV2RM0');
